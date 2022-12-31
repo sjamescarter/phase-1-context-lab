2837490ee72baa6fe87a22600dbb4c648091af85
sjamescarter <stephenjamescarter@gmail.com>
2022-12-31 17:08:05 -0600

    commit tree 023256a8c1ff3db6870e3540900106a9eaba0de9

--- index.js
+++ index.js
@@ -21,3 +21,69 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord(record) {
+    let employeeRecord = {
+        firstName: record[0],
+        familyName: record[1],
+        title: record[2],
+        payPerHour: record[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employeeRecord;
+}
+
+function createEmployeeRecords(array) {
+    let employeeRecords = []
+    array.map(record => employeeRecords.push(createEmployeeRecord(record)))
+    return employeeRecords;
+}
+
+function createTimeInEvent(dateStamp) {
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(dateStamp.split(' ')[1]),
+        date: dateStamp.split(' ')[0]
+    })
+    return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(dateStamp.split(' ')[1]),
+        date: dateStamp.split(' ')[0]
+    })
+    return this;
+}
+
+function hoursWorkedOnDate(dateStamp) {
+    let startTime, endTime, hoursWorked;
+    this.timeInEvents.find( function (e) {
+        if(e.date === dateStamp){
+            startTime = e.hour
+        }
+    })
+    this.timeOutEvents.find( function (e) {
+        if(e.date === dateStamp){
+            endTime = e.hour
+        }
+    })
+    hoursWorked = (endTime - startTime) / 100
+    return hoursWorked;
+}
+
+function wagesEarnedOnDate(dateStamp) {
+    let payOwed = this.payPerHour * hoursWorkedOnDate.call(this, dateStamp)
+    return payOwed;
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(element => element.firstName === firstName)
+}
+
+function calculatePayroll(employeeRecords) {
+    let payroll = []
+    employeeRecords.forEach(employee => payroll.push(allWagesFor.call(employee)))
+    return payroll.reduce((acc, value) => acc + value)
+}
